b641f1243c19afc0d2763c8f631cefe36a40ffd4
cankaturkmen <cturkmen@binghamton.edu>
2023-03-21 14:20:32 -0400

    googel sign in implemented

--- lib/firebase_services/firebase_get.dart
+++ lib/firebase_services/firebase_get.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:krch_chat_app/firebase_services/firebase_service.dart';
 import 'package:krch_chat_app/models/users.dart';
 
@@ -5,9 +6,12 @@ class FirebaseGet extends FirebaseService {
   Future<Users> getUserById(String uid) async {
     Users user = Users();
 
-    await users.where('uid', isEqualTo: uid).get().then((value) {
+    await users.where('uid', isEqualTo: uid).get().then((QuerySnapshot value) {
+      print(value.docs[0].toString());
       if (value.size != 0) {
         user = Users.fromFirestore(value.docs[0]);
+        print(user.email);
+        return user;
       } else {
         // TODO throw exeption
       }
